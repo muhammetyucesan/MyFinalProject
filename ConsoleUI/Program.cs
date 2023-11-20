0d759481967811a48501b123aa252bb0f452658f
muhammetyucesan <muhammetyucesan@hotmail.com>
2023-11-20 03:41:17 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,44 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+class Program
+{   //SOLID
+    //Open Closed Principle
+    public static void Main(string[] args)
+    {
+        ProductTest();
+        //data transformation object DTO
+        //IoC
+        //CategoryTest();  //gerekince açıcaz
+
+    }
+
+    private static void CategoryTest()
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+        foreach (var category in categoryManager.GetAll())
+        {
+            Console.WriteLine(category.CategoryName);
+        }
+    }
+  
+
+    private static void ProductTest()
+    {
+        ProductManager productManager = new ProductManager(new EfProductDal());
+        var result = productManager.GetProductDetails();
+        if(result.Success == true) 
+        {
+            foreach (var product in result.Data)
+            {
+                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+            }
+        }
+        else
+        {
+            Console.WriteLine(result.Message);
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,84 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal //abstract dosyasında yaptığımız internali buraya(yani classa yani concrete deki classa) ekliyoruz ve sonra implement ediyoruz ampulden.  
+    {
+        List<Product> _products;
+        public InMemoryProductDal() //ctor yazup tabtab yaptık constructor oluşturduk.
+        {
+            _products = new List<Product>
+            {
+                new Product { ProductId = 1, CategoryId = 1 ,ProductName="Bardak", UnitPrice= 15 , UnitsInStock = 15 },
+                new Product { ProductId = 2, CategoryId = 1, ProductName = "Kamera", UnitPrice = 500, UnitsInStock = 3 },
+                new Product { ProductId = 3, CategoryId = 2, ProductName = "Telefon", UnitPrice = 1500, UnitsInStock = 2 },
+                new Product { ProductId = 4, CategoryId = 2, ProductName = "Klavye", UnitPrice = 150, UnitsInStock = 65 } ,
+                new Product { ProductId = 5, CategoryId = 2, ProductName = "Bardak",UnitPrice = 15, UnitsInStock = 15 }
+            };
+
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            //       Product productToDelete = null; //referansı yok diye eşit null dedik. = new ... yapmıyoruz yani referans atamaya gerek yok tekrar
+            //       foreach (var p in _products)
+            //       {
+            //           if(product.ProductId == p.ProductId)
+            //       {
+            //       productToDelete = p;
+            //   }
+
+            Product productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId);  // p takma isim. bu kod yukardaki foreach ve if kısımlarını yapıyor direkt. Kodu basitleştirdik.
+            _products.Remove(productToDelete);
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            //gönderdiğim ürün id sine sahip olan listedeki ürünü bul demek.
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);  // p takma isim. bu kod yukardaki foreach ve if kısımlarını yapıyor direkt. Kodu basitleştirdik.
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+        }
+
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
